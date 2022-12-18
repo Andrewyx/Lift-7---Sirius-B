61b508d2b742075b0ac6b58b28162ddc1d9132ee
Andrew Mao <andrewyxmao@gmail.com>
2022-12-17 23:27:45 -0800

    MC Movement + Jump
    Still missing single-jump programming. Currently, infinite flight is possible...

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rigbod;
+    
+    
+    private void Start()
     {
-        Debug.Log("Hello, World!");
+        Debug.Log("Hello, World!"); 
+        rigbod = GetComponent<Rigidbody2D>();
+                
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        float xdirect = Input.GetAxisRaw("Horizontal");
+        rigbod.velocity = new Vector2(xdirect * 8f, rigbod.velocity.y);
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigbod.velocity = new Vector3(rigbod.velocity.x,  15f, 0);
+        }
         
     }
 }
