ac5c13ffe9c7bf321f0c118d6ba9f1e8205957a5
TMZero-c <alexmeimarcus@gmail.com>
2022-12-20 04:09:45 -0800

    Revert "Merge branch 'master' into Audio"
    This reverts commit 579d9376f9615d132e07ad593f8d368eed707b8c, reversing
    changes made to c5e27b8b1ef8c905f13eb7603ca3213730dc3a7d.

--- Assets/Enemies/AIChase.cs
+++ Assets/Enemies/AIChase.cs
@@ -20,7 +20,7 @@ public class AIChase : MonoBehaviour
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
         direction.Normalize();
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         if (distance > distanceSonic)
         {
